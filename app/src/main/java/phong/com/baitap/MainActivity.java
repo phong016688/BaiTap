c57cf2909b839d1fd9918ef4d84b9de4516a3a6b
phong016688 <phong016688@gmail.com>
2018-09-14 14:49:55 +0700

    bai 2

--- app/src/main/java/phong/com/baitap/MainActivity.java
+++ app/src/main/java/phong/com/baitap/MainActivity.java
@@ -1,94 +1,64 @@
 package phong.com.baitap;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String [] arr = new String[] { "AN GIANG",
-                         "BÀ RỊA - VŨNG TÀU",
-                         "BẮC GIANG",
-                         "BẮC KẠN",
-                         "BẠC LIÊU",
-                         "BẮC NINH",
-                         "BẾN TRE",
-                         "BÌNH ĐỊNH",
-                         "BÌNH DƯƠNG",
-                         "BÌNH PHƯỚC",
-                         "BÌNH THUẬN",
-                         "CÀ MAU",
-                         "CAO BẰNG",
-                         "ĐẮK LẮK",
-                         "ĐẮK NÔNG",
-                         "ĐIỆN BIÊN",
-                         "ĐỒNG NAI",
-                         "ĐỒNG THÁP",
-                         "GIA LAI",
-                         "HÀ GIANG",
-                         "HÀ NAM",
-                         "HÀ TĨNH",
-                         "HẢI DƯƠNG",
-                         "HẬU GIANG",
-                         "HÒA BÌNH",
-                         "HƯNG YÊN",
-                         "KHÁNH HÒA",
-                         "KIÊN GIANG",
-                         "KON TUM",
-                         "LAI CHÂU",
-                         "LÂM ĐỒNG",
-                         "LẠNG SƠN",
-                         "LÀO CAI",
-                         "LONG AN",
-                         "NAM ĐỊNH",
-                         "NGHỆ AN",
-                         "NINH BÌNH",
-                         "NINH THUẬN",
-                         "PHÚ THỌ",
-                         "QUẢNG BÌNH",
-                          "QUẢNG NAM",
-                         "QUẢNG NGÃI",
-                         "QUẢNG NINH",
-                         "QUẢNG TRỊ",
-                         "SÓC TRĂNG",
-                         "SƠN LA",
-                         "TÂY NINH",
-                         "THÁI BÌNH",
-                         "THÁI NGUYÊN",
-                         "THANH HÓA",
-                         "THỪA THIÊN HUẾ",
-                         "TIỀN GIANG",
-                         "TRÀ VINH",
-                         "TUYÊN QUANG",
-                         "VĨNH LONG",
-                         "VĨNH PHÚC",
-                         "YÊN BÁI",
-                         "PHÚ YÊN",
-                         "ĐÀ NẴNG",
-                         "HẢI PHÒNG",
-                         "HÀ NỘI",
-                         "TP HCM",
-                         "CẦN THƠ"};
-        ListView lv = findViewById(R.id.listview2);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arr);
-        lv.setAdapter(adapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        init();
+
+
+    }
+    public void init(){
+        final EditText eda = findViewById(R.id.editText);
+        final EditText edb = findViewById(R.id.editText2);
+        final EditText edc = findViewById(R.id.editText3);
+        final TextView textViewkq = findViewById(R.id.textView5);
+        Button buttongiai = findViewById(R.id.button1);
+        Button buttonxoa = findViewById(R.id.button2);
+
+        buttongiai.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                 intent.putExtra(Intent.EXTRA_COMPONENT_NAME, arr[position]);
-                 startActivity(intent);
+            public void onClick(View v) {
+                if(eda.getText().toString().equals("") || edb.getText().toString().equals("") || edc.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "bạn nhập thiếu hệ số", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    int a = Integer.parseInt(eda.getText().toString());
+                    int b = Integer.parseInt(edb.getText().toString());
+                    int c = Integer.parseInt(edc.getText().toString());
+                    int w = b -c;
+                    if(a== 0) {
+                        if (w == 0)
+                            textViewkq.setText("Phương trình có vô số nghiệm");
+                        else
+                            textViewkq.setText("phương trình vô nghiệm");
+                    }else {
+                        if(w!=0) textViewkq.setText("x = " + -w +"/" +a);
+                        else textViewkq.setText("x = 0");  }
+                }
 
             }
         });
+        buttonxoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eda.setText("");
+                edb.setText("");
+                edc.setText("");
+                textViewkq.setText("");
+            }
+        });
+
+
 
     }
 
